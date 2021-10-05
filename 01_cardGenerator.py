18d1d99893a0bcfafd44d67dcdc6d2d1e61f0160
Augustin <augustin.briolon@gmail.com>
2021-10-05 22:56:50 +0200

    ex 3 and 1

--- 01_cardGenerator.py
+++ 01_cardGenerator.py
@@ -1,2 +1,9 @@
 import random
 
+numbers = []
+
+i = 0
+while i <= 8:
+    i += 1
+    random_number = (random.randint(0, 9))
+
--- 03_pinCode.py
+++ 03_pinCode.py
@@ -1,1 +1,15 @@
-qq
+import hashlib
+
+hash = "8a069869956a4e0cf7ac69f9c20e0d49"
+
+i = 72000000
+while i <= 99999999:
+    print(i)
+    iHash = hashlib.md5(str(i).encode())
+    print(iHash.hexdigest())
+    if iHash.hexdigest() == hash:
+        print(f"We find the password, it's : {i}" )
+        break
+    elif i == 99999999:
+        print('Password not find')
+    i += 1
