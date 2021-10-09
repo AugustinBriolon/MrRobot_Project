8c53994074e8dacef10b91cd599c51e3bd29bc86
Augustin <augustin.briolon@gmail.com>
2021-10-09 16:50:36 +0200

    ex 1 and 3

--- 01_cardGenerator.py
+++ 01_cardGenerator.py
@@ -1,9 +1,41 @@
 import random
+import sys
 
-numbers = []
+type = input("Vista or Mastercard only >").lower()
 
-i = 0
-while i <= 8:
-    i += 1
-    random_number = (random.randint(0, 9))
+def card_generator(number=0):
 
+    card = list("0000000000000000")
+
+    for i in range(16):
+
+        number = random.randrange(0, 9)
+
+        card[i] = card[i].replace("0", str(number))
+
+    if type == "visa":
+        card[0] = '4'
+        new_card = "".join(card)
+    elif type == "mastercard":
+        card[0] = '5'
+        new_card = "".join(card)
+    else:
+        print("Error with your type of card (use only visa or mastercard)")
+        sys.exit()
+
+    new_card = new_card[::-1]
+    sum = 0
+    for i in range(1, 16, 2):
+        mult = int(new_card[i]) *2
+        if mult > 9:
+            mult -= 9
+        sum += mult
+    for i in range(0, 16, 2):
+        mult2 = int(new_card[i])
+        sum += mult2
+    if sum%10 == 0:
+        return new_card[::-1]
+    else:
+        return card_generator()
+
+print(f"Your card number is : {card_generator()}")
